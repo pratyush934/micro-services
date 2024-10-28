b1eb1592c939af5f5ab32bf45614f0720264aeeb
Pratyush <oggycandle786@gmail.com>
2024-10-29 02:15:05 +0530

    Adding pain in the ass

--- services/config-server/src/main/java/com/conifgserver/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/com/conifgserver/configserver/ConfigServerApplication.java
@@ -1,14 +1,16 @@
-package com.hotelservice.hotelservice;
+package com.conifgserver.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class HotelServiceApplication {
+@EnableConfigServer
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-			SpringApplication.run(HotelServiceApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
 }
